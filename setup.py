0e6b483f73a158d40ea96b979b1b5e8d2f8454f7
Alisia Fadini <alisia.fadini15@imperial.ac.uk>
2024-02-01 10:13:19 +0000

    make_diffmap in setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,30 @@
-
-
-DESCRIPTION      =   "Scripts for crystallographic electron density maps containing low occupancy species"
+DESCRIPTION = "Scripts for crystallographic electron density maps containing low occupancy species"
 LONG_DESCRIPTION = """
 METEOR contains commandline scripts and functions for finding low occupancy species in crystallographic maps.
 This module will be part of reciprocalspaceship and its "booster" package, rs-booster.
 """
 
 try:
-	from setuptools import setup, find_packages
+    from setuptools import setup, find_packages
 
 except ImportError:
-	from disutils.core import setup
+    from disutils.core import setup
 
-    
+
 setup(
-    name='meteor',
+    name="meteor",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    author='Alisia Fadini',
+    author="Alisia Fadini",
     author_email="alisia.fadini15@imperial.ac.uk",
     install_requires=["reciprocalspaceship", "matplotlib", "seaborn"],
     packages=find_packages(),
     entry_points={
-      "console_scripts": [
-        "meteor.bckgr_subtract=meteor.background_subtraction.make_Nbgmax_map:main",
-        "meteor.tv_denoise=meteor.TV_filtering.tv_denoise_map:main",
-        "meteor.iterative_TV=meteor.TV_filtering.iterative_tv:main"
+        "console_scripts": [
+            "meteor.bckgr_subtract=meteor.background_subtraction.make_Nbgmax_map:main",
+            "meteor.diffmap=meteor.background_subtraction.make_diffmap:main",
+            "meteor.tv_denoise=meteor.TV_filtering.tv_denoise_map:main",
+            "meteor.iterative_TV=meteor.TV_filtering.iterative_tv:main",
         ]
-  }
+    },
 )
