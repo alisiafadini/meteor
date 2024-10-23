3cc8171ef5c143f9e77a8ac43fe5adf81946be19
tjlane <thomas.joseph.lane@gmail.com>
2024-10-23 08:38:35 +0100

    testing update

--- test/conftest.py
+++ test/conftest.py
@@ -4,5 +4,10 @@ import pytest
 
 
 @pytest.fixture(scope="session")
-def test_data_dir() -> Path:
+def data_dir() -> Path:
     return Path(__file__).parent.resolve() / "data"
+
+
+@pytest.fixture(scope="session")
+def example_pdb_file(data_dir: Path) -> Path:
+    return data_dir / "rsEGFP2_dark_no-chromophore.pdb"
