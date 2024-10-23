494aabc934e56ac73e72b5ea5f6bbeb6fa2b8e7c
tjlane <thomas.joseph.lane@gmail.com>
2024-10-23 09:37:16 +0100

    stricter mypy, helped root out a few issues

--- test/unit/test_testing.py
+++ test/unit/test_testing.py
@@ -4,7 +4,7 @@ import pytest
 from meteor import testing as mt
 
 
-def test_phases_allclose():
+def test_phases_allclose() -> None:
     close1 = np.array([0.0, 89.9999, 179.9999, 359.9999, 360.9999])
     close2 = np.array([-0.0001, 90.0, 180.0001, 360.0001, 0.9999])
     far = np.array([0.5, 90.5, 180.0, 360.0, 361.0])
