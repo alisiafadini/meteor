c54e50b470a8d7a69b306b7f817ca1e04fa4e2ec
tjlane <thomas.joseph.lane@gmail.com>
2024-10-25 22:17:38 +0100

    fix find replace mishap

--- meteor/io.py
+++ meteor/io.py
@@ -7,31 +7,31 @@ from __future__ import annotations
 import re
 from typing import Final
 
-OBSERVED_INTENSITY_columnS: Final[list[str]] = [
+OBSERVED_INTENSITY_COLUMNS: Final[list[str]] = [
     "I",  # generic
     "IMEAN",  # CCP4
     "I-obs",  # phenix
 ]
 
-OBSERVED_AMPLITUDE_columnS: Final[list[str]] = [
+OBSERVED_AMPLITUDE_COLUMNS: Final[list[str]] = [
     "F",  # generic
     "FP",  # CCP4 & GLPh native
     r"FPH\d",  # CCP4 derivative
     "F-obs",  # phenix
 ]
 
-OBSERVED_UNCERTAINTY_columnS: Final[list[str]] = [
+OBSERVED_UNCERTAINTY_COLUMNS: Final[list[str]] = [
     "SIGF",  # generic
     "SIGFP",  # CCP4 & GLPh native
     r"SIGFPH\d",  # CCP4
 ]
 
-COMPUTED_AMPLITUDE_columnS: Final[list[str]] = ["FC"]
+COMPUTED_AMPLITUDE_COLUMNS: Final[list[str]] = ["FC"]
 
-COMPUTED_PHASE_columnS: Final[list[str]] = ["PHIC"]
+COMPUTED_PHASE_COLUMNS: Final[list[str]] = ["PHIC"]
 
 
-class AmbiguousMtzcolumnError(ValueError): ...
+class AmbiguousMtzColumnError(ValueError): ...
 
 
 def _infer_mtz_column(columns_to_search: list[str], columns_to_look_for: list[str]) -> str:
@@ -44,11 +44,11 @@ def _infer_mtz_column(columns_to_search: list[str], columns_to_look_for: list[st
     if len(matches) == 0:
         msg = "cannot infer MTZ column name; "
         msg += f"cannot find any of {columns_to_look_for} in {columns_to_search}"
-        raise AmbiguousMtzcolumnError(msg)
+        raise AmbiguousMtzColumnError(msg)
     if len(matches) > 1:
         msg = "cannot infer MTZ column name; "
         msg += f">1 instance of {columns_to_look_for} in {columns_to_search}"
-        raise AmbiguousMtzcolumnError(msg)
+        raise AmbiguousMtzColumnError(msg)
 
     [match] = matches
     if match is None:
@@ -58,21 +58,21 @@ def _infer_mtz_column(columns_to_search: list[str], columns_to_look_for: list[st
     return match.group(0)
 
 
-def find_observed_intensity_column(mtz_column_columns: list[str]) -> str:
-    return _infer_mtz_column(mtz_column_columns, OBSERVED_INTENSITY_columnS)
+def find_observed_intensity_column(mtz_columns: list[str]) -> str:
+    return _infer_mtz_column(mtz_columns, OBSERVED_INTENSITY_COLUMNS)
 
 
-def find_observed_amplitude_column(mtz_column_columns: list[str]) -> str:
-    return _infer_mtz_column(mtz_column_columns, OBSERVED_AMPLITUDE_columnS)
+def find_observed_amplitude_column(mtz_columns: list[str]) -> str:
+    return _infer_mtz_column(mtz_columns, OBSERVED_AMPLITUDE_COLUMNS)
 
 
-def find_observed_uncertainty_column(mtz_column_columns: list[str]) -> str:
-    return _infer_mtz_column(mtz_column_columns, OBSERVED_UNCERTAINTY_columnS)
+def find_observed_uncertainty_column(mtz_columns: list[str]) -> str:
+    return _infer_mtz_column(mtz_columns, OBSERVED_UNCERTAINTY_COLUMNS)
 
 
-def find_computed_amplitude_column(mtz_column_columns: list[str]) -> str:
-    return _infer_mtz_column(mtz_column_columns, COMPUTED_AMPLITUDE_columnS)
+def find_computed_amplitude_column(mtz_columns: list[str]) -> str:
+    return _infer_mtz_column(mtz_columns, COMPUTED_AMPLITUDE_COLUMNS)
 
 
-def find_computed_phase_column(mtz_column_columns: list[str]) -> str:
-    return _infer_mtz_column(mtz_column_columns, COMPUTED_PHASE_columnS)
+def find_computed_phase_column(mtz_columns: list[str]) -> str:
+    return _infer_mtz_column(mtz_columns, COMPUTED_PHASE_COLUMNS)
