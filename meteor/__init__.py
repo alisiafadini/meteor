a6cc43370b1113c2d7d3df5a44e357aa941a8573
tjlane <thomas.joseph.lane@gmail.com>
2024-11-01 16:04:29 +0000

    oh yes, it is good

--- meteor/__init__.py
+++ meteor/__init__.py
@@ -1,3 +1,9 @@
+"""
+`meteor` is a tool for computing crystallographic difference maps
+
+.. include:: ./documentation.md
+"""
+
 __all__ = ["__version__", "version"]
 
 from ._version import __version__, version
---
+++ meteor/documentation.md
@@ -0,0 +1,1 @@
+This is a TEST hellooo world
